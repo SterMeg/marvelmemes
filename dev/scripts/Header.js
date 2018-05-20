86d73aad1552970bfd9553506f14fb9924e7aafe
Meghan Sterling <sterling.meghan@gmail.com>
2018-05-19 21:13:54 -0400

    Added responsive header styles

--- dev/scripts/Header.js
+++ dev/scripts/Header.js
@@ -5,7 +5,16 @@ const Header = () => {
         <header>
             <div className="wrapper">
                 <h1>Marvel at my dad jokes!</h1>
-
+                <div className="image-container">
+                    <img src="../../public/assets/lightning.svg" alt=""/>
+                </div>
+                <div className="display-box">
+                    <h2 className="heading-left">Marvel characters</h2>
+                    <h2 className="heading-right">Dad Jokes</h2>
+                </div>
+                <p>Choose your character! Get a dad joke!</p>
+                <p>Customize your joke card!</p>
+                <button>Let's Do It!</button>
             </div>
         </header>
     )
